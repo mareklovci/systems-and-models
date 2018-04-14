eaff325bf2923fb2158c94c8920dd166a40ba8f6
mareklovci <mareklovci@gmail.com>
2018-04-14 09:05:28 +0200

    Basic script
    Added basic scipt with constants initialized

---
+++ main.m
@@ -0,0 +1,9 @@
+%% Semestralni prace - Systemy a modely
+% Vypracovani semestralni prace - inicializace promennych
+
+m = 10; % kg - hmostnost zavazi
+M = 2; % kg - hmostnost voziku
+l_10 = 1; % m - klidova delka pruziny
+k = 5; % tuhost pruziny
+b = 0.7; % tuhost tlumice
+force = 10; % N - sila pusobici ve smeru vektoru u
