de20d331319a91c69fdbf5f2e278f7dedfb82c8e
mareklovci <mareklovci@gmail.com>
2018-04-27 20:28:52 +0200

    Working models :blush:

---
+++ data2graphs.m
@@ -0,0 +1,34 @@
+function [ ] = data2graphs( data, type )
+%DATA2GRAPHS Summary of this function goes here
+%   Detailed explanation goes here
+
+if strcmp(type, 'simscape')
+    name = 'Simscape';
+elseif strcmp(type, 'diff')
+    name = 'Differential equations';
+end
+    
+time = data.Time;
+positionU = data.Data(:,1);
+velocityU = data.Data(:,2);
+positionY = data.Data(:,3);
+velocityY = data.Data(:,4);
+
+figure
+
+subplot(2,1,1);
+plot(time, positionU, 'r', time, positionY, 'b')
+title([name, ' model position'])
+ylabel('Position')
+xlabel('Time')
+legend('Cart u', 'Cart y')
+
+subplot(2,1,2);
+plot(time, velocityU, 'r', time, velocityY, 'b')
+title([name, ' model velocity'])
+ylabel('Velocity')
+xlabel('Time')
+legend('Cart u', 'Cart y')
+
+end
+
--- main.m
+++ main.m
@@ -6,12 +6,15 @@ M = 2; % kg - hmostnost voziku
 l10 = 1; % m - klidova delka pruziny
 k = 5; % tuhost pruziny
 b = 0.7; % tuhost tlumice
-Fe = 10; % N - sila pusobici ve smeru vektoru u
+Fe = 0; % N - sila pusobici ve smeru vektoru u
 
 u0 = 0; 
 du0 = 0; 
 y0 = 2*l10; 
 dy0 = 0; 
 
-simout = sim('model2');
-plot(simout)
+sim('model1')
+data2graphs(simscape, 'simscape')
+
+sim('model2')
+data2graphs(differential, 'diff')
